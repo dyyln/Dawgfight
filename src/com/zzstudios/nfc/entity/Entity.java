ebe9923624522446a9025264e29b4e7c2cba2328
Dylan Dhokia <dylandhokia@gmail.com>
2013-07-05 22:26:03 +0100

    Added Entity Class

---
+++ src/com/zzstudios/nfc/entity/Entity.java
@@ -0,0 +1,19 @@
+package com.zzstudios.nfc.entity;
+
+import com.zzstudios.nfc.assets.Screen;
+import com.zzstudios.nfc.input.InputHandler;
+import com.zzstudios.nfc.level.Level;
+
+public class Entity {
+	public int x, y, rotation;
+	
+	public int xSize, ySize;
+	
+	public void tick(Level level, InputHandler input){
+		
+	}
+	
+	public void render(Screen screen, Level level, int xScroll){
+		
+	}
+}
